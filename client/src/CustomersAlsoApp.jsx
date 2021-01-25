7eb6598b67b3ace2be60a52980a0241f9b1b852c
AntoineBalaine <abalaineaccts@gmail.com>
2021-01-25 13:33:24 -0800

    working carousel / first implementation

---
+++ client/src/CustomersAlsoApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Slidee2 from './components/carousel2.jsx';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      {/* <Slidee /> */}
+      <Slidee2 />
+    </div>
+  );
+}
+
+export default App;
